f2318a1c924ac948b6ddecc6e1940c04722a32f7
donotr <donovan.tobar16@gmail.com>
2023-10-03 21:20:45 -0600

    fix: Se agrego un ontap para esconder el teclado de busqueda

--- lib/presentation/barrabusqueda.dart
+++ lib/presentation/barrabusqueda.dart
@@ -5,61 +5,64 @@ class OurSearchBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-              padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-              child:SingleChildScrollView(
-                reverse: true,
-                child:
-              Column(
-                children: [
-                  SearchAnchor(builder:
-                          (BuildContext context, SearchController controller) {
-                    return SearchBar(
-                      controller: controller,
-                      padding: const MaterialStatePropertyAll<EdgeInsets>(
-                          EdgeInsets.symmetric(horizontal: 16.0)),
-                      onTap: () {
-                        controller.openView();
-                      },
-                      onChanged: (_) {
-                        controller.openView();
-                      },
-                      leading: const Icon(Icons.search),
-                      trailing: <Widget>[
-                        Tooltip(
-                          message: 'Change filters',
-                          child: IconButton(
-                            onPressed: () {
-                              //settear filtro
-                              setState(() {});
-                            },
-                            icon: const Icon(Icons.filter_list_alt),
-                            color: Color.fromARGB(216, 107, 45, 117),
-                          ),
-                        )
-                      ],
-                    );
-                  },
-                      // Aqui muestra sugerencias de busqueda
-
-                      suggestionsBuilder:
-                          (BuildContext context, SearchController controller) {
-                    return List<ListTile>.generate(5, (int index) {
-                      final String item = 'item $index';
-                      return ListTile(
-                        title: Text(item),
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+                child: Container(
+                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child:SingleChildScrollView(             
+                  reverse: true,
+                  child:
+                Column(
+                  children: [
+                    SearchAnchor(builder:
+                            (BuildContext context, SearchController controller) {
+                      return SearchBar(
+                        controller: controller,
+                        padding: const MaterialStatePropertyAll<EdgeInsets>(
+                            EdgeInsets.symmetric(horizontal: 16.0)),
                         onTap: () {
-                          setState(() {
-                            controller.closeView(item);
-                          });
+                          controller.openView();
                         },
+                        onChanged: (_) {
+                          controller.openView();
+                        },
+                        leading: const Icon(Icons.search),
+                        trailing: <Widget>[
+                          Tooltip(
+                            message: 'Change filters',
+                            child: IconButton(
+                              onPressed: () {
+                                //settear filtro
+                                setState(() {});
+                              },
+                              icon: const Icon(Icons.filter_list_alt),
+                              color: const Color.fromARGB(216, 107, 45, 117),
+                            ),
+                          )
+                        ],
                       );
-                    });
-                  }),
-                ],
+                    },
+                        // Aqui muestra sugerencias de busqueda
+    
+                        suggestionsBuilder:
+                            (BuildContext context, SearchController controller) {
+                      return List<ListTile>.generate(5, (int index) {
+                        final String item = 'item $index';
+                        return ListTile(
+                          title: Text(item),
+                          onTap: () {
+                            setState(() {
+                              controller.closeView(item);
+                            });
+                          },
+                        );
+                      });
+                    }),
+                  ],
+                ),
+                )
               ),
-              )
-            );
+    );
   }
   
   void setState(Null Function() param0) {}
